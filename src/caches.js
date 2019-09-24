482d6640fecc88c630682982d99d6770f7c3fbe5
Brooks McKinley <brooksmckinley@yahoo.com>
2019-09-24 16:21:09 -0400

    Added caches.pull()

--- src/caches.js
+++ src/caches.js
@@ -1,6 +1,14 @@
-var fs = require("fs").promises,
-	util = require("util");
+var fs = require("fs"),
+	crypto = require("crypto");
 var ytdl = require("./ytdl.js");
+
+function getFilename(url) {
+	fs.mkdirSync("caches", { recursive:true }); // make sure caches directory exists
+	let sha = crypto.createHash("sha1");
+	sha.update(url);
+	let filename = "caches/" + sha.digest("hex");
+	return filename;
+}
 
 exports.cleanup = async function() {
 	
@@ -8,3 +16,14 @@ exports.cleanup = async function() {
 	setTimeout(exports.cleanup, 100000);
 }
 
+exports.pull = async function(url) {
+	let filename = getFilename(url);
+	try {
+		await fs.promises.stat(filename); // if this fails the catch block goes
+		return filename;
+	}
+	catch {
+		ytdl.fetch(url, filename);
+		return filename;
+	}
+}
