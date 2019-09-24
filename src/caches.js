b287c97bbdd43b59e20d8073a949462a1e836634
Brooks McKinley <brooksmckinley@yahoo.com>
2019-09-24 17:49:49 -0400

    Add cleanup routine

--- src/caches.js
+++ src/caches.js
@@ -10,8 +10,24 @@ function getFilename(url) {
 	return filename;
 }
 
-exports.cleanup = async function() {
-	
+exports.cleanup = function() {
+	console.info("[DEBUG] Cleanup started.");
+	let timeout = JSON.parse(fs.readFileSync("config.json")).cache;
+	let minTime = Date.now() - (timeout * 1000);
+	if (!timeout) return; // If it's indefinite, return immediately
+	fs.readdir("caches", (err, files) => {
+		if (err) {
+			console.warn("[WARN] Cleanup failed: " + err);
+			return;
+		}
+		for (let file of files) {
+			let stat = fs.statSync("caches/" + file);
+			if (stat.mtimeMs < minTime) {
+				console.debug("[DEBUG] Removing " + file);
+				fs.unlinkSync("caches/" + file);
+			}
+		}
+	});
 	// Schedule next cleanup
 	setTimeout(exports.cleanup, 100000);
 }
@@ -19,7 +35,8 @@ exports.cleanup = async function() {
 exports.pull = async function(url) {
 	let filename = getFilename(url);
 	try {
-		await fs.statSync(filename); // if this fails the catch block goes
+		fs.statSync(filename); // if this fails the catch block goes
+		fs.utimesSync(filename, Date.now() / 1000, Date.now() / 1000); // Touch file for cleanup routine
 		return filename;
 	}
 	catch {
