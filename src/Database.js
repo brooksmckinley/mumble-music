e130b25d6c3144f0ce568956eb863bb2c1e39a2d
Brooks McKinley <brooksmckinley@yahoo.com>
2019-10-19 17:59:11 -0400

    Added base db

--- src/Database.js
+++ src/Database.js
@@ -1,7 +1,16 @@
 var fs = require('fs');
 
 function Database(name) {
-	
+	this.name = name;
+	try {
+		this.db = JSON.parse(fs.readFileSync(name));
+	}
+	catch {
+		this.db = {};
+		this.db.playlists = [];
+		this.db.songs = [];
+		this._save();
+	}
 }
 
 Database.prototype.increment = function(song) {
@@ -14,6 +23,10 @@ Database.prototype.getTopTenSongs = function(user) {
 }
 
 Database.prototype.getTopTenPlaylists = function(user) {
+}
+
+Database.prototype._save = function() {
+	fs.writeFile(this.name, JSON.stringify(this.db));
 }
 
 module.exports = Database;
