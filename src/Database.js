4ef458e7a38d8f5e30c802c94ad28e88e971829f
Brooks McKinley <brooksmckinley@yahoo.com>
2019-10-19 17:41:20 -0400

    Created API

---
+++ src/Database.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+
+function Database() {
+
+}
+
+Database.prototype.increment = function(song) {
+}
+
+Database.prototype.getSong = function(song) {
+}
+
+Database.prototype.getTopTenSongs = function(user) {
+}
+
+Database.prototype.getTopTenPlaylists = function(user) {
+}
+
+module.exports = Database;
